6941a6c746249a4219fe69f3920418c6695a2983
hiworzz2HK <erdalkanmaz50@gmail.com>
2023-02-20 15:53:22 +0100

    premier changement avec vscode

--- modules/header.php
+++ modules/header.php
@@ -7,9 +7,6 @@
             <h3><?php echo($titrePage) ?></h3>
         </div>
         <div class="user">
-           <form method="get" action="../controller/catalog.php">
-            <label for="pseudo">Votre nom :</label><input type="text" id="pseudo" name="nom"/>
-            <input type="submit" value="Envoyer"/>
-           </form>
+          
         </div>
 </header>
--- view/catalogView.php
+++ view/catalogView.php
@@ -11,11 +11,8 @@
             require("../modules/nav.php");
             require("../modules/header.php");
 
-            if (isset($_GET['nom'])) {
-                echo(htmlspecialchars('bonjour '.$_GET['nom']));
-            } else {
-                echo("Bonjour inconnu");
-            }
+           
+            
         ?>
     </body>
 </html>
